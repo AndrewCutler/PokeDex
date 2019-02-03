e27c99c0c8c7f640758382bb6bf26952d0a2152d
Andrew Cutler <andrewkcutler@gmail.com>
2019-02-03 15:33:03 -0500

    Added type pages and started delete route

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //import libraries
 const express = require('express'),
       mongoose = require('mongoose'),
-      bodyParser = require('body-parser');
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override');
 
 //import pokemon model
 const Pokemon = require('./models/Pokemon');
@@ -21,10 +22,8 @@ mongoose
   .then(console.log('connected to MongoDB'))
   .catch(err => console.log(err));
 
-
 //set view engine
 app.set('view engine', 'ejs');
-
 
 //GET routes
 //index/home page
@@ -47,6 +46,19 @@ app.get('/pokedex',(req,res) => {
 app.get('/pokedex/new',(req,res) => {
   res.render('new');
 });
+
+//SHOW by ID
+app.get('/pokedex/:id',(req,res) => {
+  Pokemon.findById(req.params.id, (err,foundPokemon) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/pokedex');
+    } else {
+      res.render('show',{pokemon: foundPokemon});
+    }
+  });
+});
+
 
 
 //POST routes
@@ -76,17 +88,49 @@ app.get('/pokedex/:id/edit',(req,res) => {
   });
 });
 
+//UPDATE route
+app.put('/pokedex/:id',(req,res) => {
+  Pokemon.findByIdAndUpdate(req.params.id, 
+      req.body.pokemon, 
+      (err, updatedPokemon) => {
+        if (err) {
+          console.log(err);
+          res.redirect('pokedex');
+        } else {
+          res.redirect('/pokedex/' + req.params.id);
+        }
+      });
+});
+
+//SHOW by type
+app.get('/:type',(req,res) => {
+  const type = req.params.type;
+  Pokemon.find({ type: type },(err,pokemon) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/');
+    } else {
+      res.render('types', {pokemon: pokemon});
+    }
+  });
+});
+
+//DELETE entry
+app.delete('/pokedex/:id',(req,res) => {
+  Pokemon.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/pokedex');
+    }
+  });
+});
+
 port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on ${port}`));
 
-//FIGURED THINGS OUT:
-// DB name: pokedex
-// collection name: pokemons
-
 // TODO:
-// allow editing on edit page
 // organize pokedex index page
 // add delete route
-// create pages for each type, showing weaknesses/strengths and 
-// all pokemon of that type
+// create pages for each type, showing weaknesses/strengths
